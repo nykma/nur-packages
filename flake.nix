010b27daa211a6c11044db274ad76268e334180b
Nyk Ma <nykma@mask.io>
2024-04-05 01:32:21 +0800

    [!] default.nix: add new package

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "My personal NUR repository";
+  description = "Nyk Ma's personal NUR repository";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   outputs = { self, nixpkgs }:
     let
