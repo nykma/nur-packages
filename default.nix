55cb507d525ede18078313048a79785ca8b9bd67
Nyk Ma <i@nyk.ma>
2024-11-02 15:19:36 +0800

    [!] pkgs/safeheron-crypto-suites: fix protobuf3_20 missing

--- default.nix
+++ default.nix
@@ -1,8 +1,14 @@
-{ pkgs ? import <nixpkgs> {
-   overlays = [
+{
+  pkgs ? import <nixpkgs> {
+    overlays = [
       (import (fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
     ];
-} }:
+  },
+  pkgs-protobuf320 ? import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/656b40c807e4c4965198a68d1f784492397fef6c.tar.gz";
+    sha256 = "sha256:0j3sxszl00kbw4i4c5dd4ip6a7f0yd3gx2y7iwqy5wkfmwp4k1fl";
+  }) { },
+}:
 
 rec {
   lib = import ./lib { inherit pkgs; };
@@ -11,8 +17,13 @@ rec {
 
   cups-detonger = pkgs.callPackage ./pkgs/cups-detonger { };
   snipaste = pkgs.callPackage ./pkgs/snipaste { };
-  safeheron-crypto-suites = pkgs.callPackage ./pkgs/safeheron-crypto-suites { };
-  multi-party-sig = pkgs.callPackage ./pkgs/multi-party-sig { inherit safeheron-crypto-suites; };
+  safeheron-crypto-suites = pkgs.callPackage ./pkgs/safeheron-crypto-suites {
+    inherit (pkgs-protobuf320) protobuf3_20;
+  };
+  multi-party-sig = pkgs.callPackage ./pkgs/multi-party-sig {
+    inherit safeheron-crypto-suites;
+    inherit (pkgs-protobuf320) protobuf3_20;
+  };
   cryptopp-cmake = pkgs.callPackage ./pkgs/cryptopp-cmake { };
   orca-slicer = pkgs.callPackage ./pkgs/orca-slicer { };
   snips-sh = pkgs.callPackage ./pkgs/snips.sh { };
--- flake.nix
+++ flake.nix
@@ -1,13 +1,14 @@
 {
   description = "Nyk Ma's personal NUR repository";
   inputs = {
+    nixpkgs-protobuf320.url = "github:NixOS/nixpkgs/656b40c807e4c4965198a68d1f784492397fef6c";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, rust-overlay }:
+  outputs = { self, nixpkgs, nixpkgs-protobuf320, rust-overlay }:
     let
       systems = [
         "x86_64-linux"
@@ -23,14 +24,18 @@
         inherit system;
         overlays = [ rust-overlay.overlays.default ];
       });
+      nixpkgs-protobuf320For = forAllSystems (system: import nixpkgs-protobuf320 {
+        inherit system;
+      });
     in
     {
       legacyPackages = forAllSystems (system:
         let
           pkgs = nixpkgsFor.${system};
+          pkgs-protobuf320 = nixpkgs-protobuf320For.${system};
         in
         import ./default.nix {
-          inherit pkgs;
+          inherit pkgs pkgs-protobuf320;
         });
       packages = forAllSystems (system:
         let
